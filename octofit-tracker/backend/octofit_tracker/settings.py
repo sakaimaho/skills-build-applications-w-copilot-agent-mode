47b14e6935d93e98034041de37a52ad4da806988
sakaimaho <sakaimaho@njk.co.jp>
2025-11-20 09:18:27 +0000

    Codespace/localhost API URL 対応: settings.py, urls.py

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -26,8 +26,12 @@ SECRET_KEY = 'django-insecure-kyx9bh)8jugruhlh5^uj%ej83c3(+bk%r5jqgf+*%8ds83+*&k
 DEBUG = True
 
 
-# Allow all hosts
-ALLOWED_HOSTS = ['*']
+# Allow localhost and Codespace URL
+import os
+CODESPACE_NAME = os.environ.get('CODESPACE_NAME')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+if CODESPACE_NAME:
+    ALLOWED_HOSTS.append(f'{CODESPACE_NAME}-8000.app.github.dev')
 
 
 # Application definition
